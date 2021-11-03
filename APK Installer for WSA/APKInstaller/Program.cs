1efc93dfc5cf4893faad02741451625d46f6c884
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-11-03 18:40:39 +0300

    Add files via upload

---
+++ APK Installer for WSA/APKInstaller/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+
+namespace APKInstaller
+{
+	static class Program
+	{
+		static string workpath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+		static string adbpath = Path.Combine(workpath, "platform-tools/adb.exe");
+		static string zippath = Path.Combine(workpath, "adb.zip");
+		static string ziplink = "https://dl.google.com/android/repository/platform-tools_r31.0.3-windows.zip";
+
+		static string path = "";
+
+		static string conaddr = "127.0.0.1:58526";
+
+		[STAThread]
+		static void Main()
+		{
+			if (!(File.Exists(adbpath)))
+			{
+				getADB();
+			}
+
+			string[] args = Environment.GetCommandLineArgs();
+			if (args.Length == 2)
+			{
+				path = args[1];
+				if (File.Exists(path) && path.EndsWith(".apk"))
+				{
+					installAPK();
+				}
+				else
+				{
+					MessageBox.Show("File is not available or invalid a Android app package. Please check the file again.", "APKInstaller for WSA");
+					Environment.Exit(1);
+				}
+
+			}
+		}
+		static void getADB()
+		{
+			if (File.Exists(zippath))
+				File.Delete(zippath);
+
+			using (var w = new WebClient())
+			{
+				w.DownloadFile(ziplink, zippath);
+			}
+
+			ZipFile.ExtractToDirectory(zippath, workpath);
+			File.Delete(zippath);
+			
+		}
+	
+		static void installAPK()
+		{
+			Process installproc = new Process();
+			ProcessStartInfo si = new ProcessStartInfo
+			{
+				FileName = adbpath,
+				Arguments = "install " + path,
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
+				CreateNoWindow = true
+			};
+
+			installproc.StartInfo = si;
+			installproc.Start();
+			installproc.WaitForExit();
+
+			string cmdout = installproc.StandardOutput.ReadToEnd();
+			if (cmdout.Contains("no devices/emulators found"))
+			{
+				connectDev();
+			}
+			else if (cmdout.Contains("Success"))
+			{
+				MessageBox.Show("Installed app successfully.", "APKInstaller for WSA");
+				Environment.Exit(0);
+			}
+			else
+			{
+				MessageBox.Show("An error occured while installing the app.", "APKInstaller for WSA");
+				Environment.Exit(1);
+			}
+		}
+
+		static void connectDev()
+		{
+			Process installproc = new Process();
+			ProcessStartInfo si = new ProcessStartInfo
+			{
+				FileName = adbpath,
+				Arguments = conaddr,
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
+				CreateNoWindow = true
+			};
+
+			installproc.StartInfo = si;
+			installproc.Start();
+			installproc.WaitForExit();
+
+			string cmdout = installproc.StandardOutput.ReadToEnd();
+			if (cmdout.Contains("connected to " + conaddr))
+			{
+				installAPK();
+			}
+			else if (cmdout.Contains("refused"))
+			{
+				MessageBox.Show("Cannot connect to WSA.", "APKInstaller for WSA");
+				Environment.Exit(1);
+			}
+			else
+			{
+				MessageBox.Show("An error occured while installing the app.", "APKInstaller for WSA");
+				Environment.Exit(1);
+			}
+		}
+	}
+}
---
+++ APK Installer for WSA/APKInstaller/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("APKInstaller")]
+[assembly: AssemblyDescription("An easy way to install APKs to WSA.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Erentronik Corp.")]
+[assembly: AssemblyProduct("APKInstaller")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66483cb3-cc46-4e18-a1e9-15f84cb5adad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
